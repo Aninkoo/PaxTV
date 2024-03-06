d5681ed68990c9eb235de0a5c8caaa8fc5aedbc2
Aninkoo <112639445+Aninkoo@users.noreply.github.com>
2024-03-06 18:07:53 +0100

    Create start.js

---
+++ start.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", 
+function countdown() {
+  if (window.location.hash){
+function countdown() {
+if ( typeof countdown.counter == 'undefined' ) {
+countdown.counter = 5; // change to required time
+}
+if(countdown.counter >= 0) {
+document.getElementById('time').style.display = 'block';
+document.getElementById('count').innerHTML = countdown.counter--;
+setTimeout(countdown, 1000);
+}
+else {
+document.getElementById('button').style.display = 'block';
+document.getElementById('Tcount').style.display = 'none';
+}
+}
+countdown();
+  }
+  else {
+    document.getElementById('time').style.display = 'none';
+  }
+});
