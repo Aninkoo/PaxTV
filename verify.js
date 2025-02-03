d4b79bc46a9817e38676682787337d29535281b5
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-02-03 16:05:19 +0100

    Update verify.js

--- verify.js
+++ verify.js
@@ -16,9 +16,9 @@ window.addEventListener("DOMContentLoaded",
 function another() { 
   if (window.location.hash){
     var start = window.location.hash.substr(1);
-    window.open(start,'_self');
   show_8870098().then(() => {
     // You need to add your user reward function here, which will be executed after the user watches the ad.
+    window.open(start,'_self');
     Telegram.WebApp.close();
     alert('✔️ You have been verified!'); 
 })
