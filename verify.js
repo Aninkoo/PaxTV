59d9ac0ca697b7fafb70c47cce2e53cd3c709ded
Aniekeme Akpan <112639445+Aninkoo@users.noreply.github.com>
2025-02-06 15:39:12 +0100

    Update verify.js

--- verify.js
+++ verify.js
@@ -17,7 +17,7 @@ function another() {
   
   show_8870098().then(() => {
     // You need to add your user reward function here, which will be executed after the user watches the ad.
-  myFun();  
+  nextt();  
   
 
 });
@@ -25,21 +25,25 @@ function another() {
 }
 
 
-
-function myFun() { 
-function countdown(myFun) {
+function nextt() { 
+function countdown() {
 if ( typeof countdown.counter == 'undefined' ) {
 countdown.counter = 3; // change to required time
 }
-if(countdown.counter <= 0) {
-function myStart() {
-  if (window.location.hash){
+if(countdown.counter >= 0) {
+document.getElementById('Ab').style.display = 'block';
+document.getElementById('Aa').style.display = 'none';
+document.getElementById('count').innerHTML = countdown.counter--;
+setTimeout(countdown, 1000);
+}
+else {
+if (window.location.hash){
     var start = window.location.hash.substr(1);
   window.open(start,'_self');
   alert('✔️ You have been verified!');
   Telegram.WebApp.close();
-};
-  };
-  countdown.counter--;
-setTimeout(countdown, 1000);
-}}}
+}
+}
+}
+countdown();
+}
